098659de822044a0f95513883642e8afdfe14812
Ramon_SFR <ramonsaviofontesrocha@gmail.com>
2024-03-18 00:26:04 -0300

    compressao css pronta

--- gruntfile.js
+++ gruntfile.js
@@ -5,13 +5,28 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'dev/styles/main.css': 'src/styles/main.less'
+                }
+            },
+            build: {
+                options: {
+                    compress: true
+                },
+                files: {
+                    'dist/styles/main.min.css': 'src/styles/main.less'
+                }
+            }
+        }
     })
 
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['less'])
+    grunt.registerTask('default', ['less:development'])
     grunt.registerTask('build', [
-        ''
+        'less:build'
     ])
 }
