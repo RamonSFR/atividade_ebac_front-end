adeb61d4fe26e468b537f9ee348732471a5c41b4
Ramon_SFR <ramonsaviofontesrocha@gmail.com>
2024-04-26 20:14:57 -0300

    classes moto and caminhao

--- main.js
+++ main.js
@@ -5,14 +5,38 @@ class Veiculo {
 }
 
 class Carro extends Veiculo {
-    constructor(marca, modelo, ano) {
+    constructor(marca, modelo, ano, cavalos) {
         super('carro');
         this.marca = marca;
         this.modelo = modelo;
         this.ano = ano;
+        this.cavalos = cavalos;
     }
 }
 
-const carro1 = new Carro('BMW', '320i', '2020');
+class Moto extends Veiculo {
+    constructor(marca, modelo, ano, cilindradas) {
+        super('moto');
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.cilindradas = cilindradas;
+    }
+}
+
+class Caminhao extends Veiculo {
+    constructor(marca, modelo, ano, cavalos) {
+        super('caminhao');
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.cavalos = cavalos;
+    }
+}
+
+const carro1 = new Carro('BMW', '320i', '2020', '184');
+
+const moto1 = new Moto('Kawasaki', 'Ninja', '2012', '300')
 
 console.log(carro1);
+console.log(moto1);
