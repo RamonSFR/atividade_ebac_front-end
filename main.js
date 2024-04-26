071315cc557980b8873eb9acd81ddf1862d880f6
Ramon_SFR <ramonsaviofontesrocha@gmail.com>
2024-04-26 20:09:51 -0300

    add class Veiculo and extend Carro

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("hello world!");
+class Veiculo {
+    constructor(categoria) {
+        this.categoria = categoria;
+    }
+}
+
+class Carro extends Veiculo {
+    constructor(marca, modelo, ano) {
+        super('carro');
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+}
+
+const carro1 = new Carro('BMW', '320i', '2020');
+
+console.log(carro1);
